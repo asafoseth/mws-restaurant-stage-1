624828675b8494702f16c926cdf96d7a12e6e6a5
asafoseth <sethasafogh@gmail.com>
2018-08-13 14:01:24 +0000

    feat: Mapbox API Token

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoiYXNhZm9zZXRoIiwiYSI6ImNqa3NjYmhjazAwNGczd254d2R2bjk2b3cifQ.KY2NrJB9uPQsWCDu8O58EQ',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
