e5e89b38d565ec3b7c0063b236ad7be0305bf3b8
asafoseth <sethasafogh@gmail.com>
2018-09-28 19:30:18 +0000

    Update: Aria
    Udate ARIA roles for DOM elements.

--- js/main.js
+++ js/main.js
@@ -165,7 +165,9 @@ createRestaurantHTML = (restaurant) => {
   address.innerHTML = restaurant.address;
   li.append(address);
 
-  const more = document.createElement('a');
+  const more = document.createElement("a");
+  more.setAttribute("role", "link");
+  more.setAttribute("aria-label", `view details of ${restaurant.name}`);
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
